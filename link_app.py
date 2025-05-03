b6307ed20b503a1c29c2c2da95d8c2ab8427cd17
ZhiyuanSue <2262387848@qq.com>
2025-05-03 10:29:09 +0800

    change the format

--- link_app.py
+++ link_app.py
@@ -7,9 +7,9 @@ link_app_file_name="link_app.S"
 
 def generate_link_app(pwd,elf_dir,elf_file_list):
 	link_app_s_file = os.path.join(pwd,link_app_file_name)
-	manager_str=".align 8\n	\
-.section .data.app\n \
-.global _num_app\n \
+	manager_str="\t.align 8\n \
+\t.section .app\n \
+\t.global _num_app\n \
 _num_app:\n"
 
 	section_str=""
